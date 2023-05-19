236887e8cb14c54a119a716c8ca2a018cb2b8f5c
Leonardo Vasconcelos Eggers <leo.v.eggers@gmail.com>
2023-05-19 18:48:34 -0300

    Encapsulando em funções. cont.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from random import choice
+from time import sleep
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
-from time import sleep
 
 
-def carregar_palavras():
+def carregar_listas():
     with open("listas/Lista-de-Palavras.txt") as plvrs:
         fcl = []
         med = []
@@ -19,10 +19,98 @@ def carregar_palavras():
             elif len(line) > 8:
                 dfcl.append(line)
         plvrs.close()
-        return fcl, med, dfcl
+    return fcl, med, dfcl
 
 
-def tent(t):
+def carregar_listas_categoria(categ):
+    if categ == '1':
+        lista_cat = "listas/atores_atrizes.txt"
+    elif categ == '2':
+        lista_cat = "listas/filmes.txt"
+    elif categ == '3':
+        lista_cat = "listas/animais.txt"
+    elif categ == '4':
+        lista_cat = "listas/paises.txt"
+    else:
+        lista_cat = "listas/artistas_bandas.txt"
+
+    with open(lista_cat, 'r', encoding='utf-8') as palavras_cat:
+        lista_escolha = []
+        for linha in palavras_cat:
+            linha = linha.upper().strip()
+            lista_escolha.append(linha)
+        plvr = choice(lista_escolha)
+        palavras_cat.close()
+    return plvr
+
+
+def num_jogadores():
+    while True:
+        md = ['1', '2']
+        num_jog = input('Escolha o número de jogadores [1/2]: ').strip()
+        if num_jog in md:
+            confirma.play()
+            break
+        print('Tente novamente.')
+        tentenovamente.play()
+    return num_jog[0]
+
+
+def carregar_palavra():
+    # Modo 2 jogadores: um dos jogadores escolhe a palavra.
+    if modo == '2':
+        plvr = input('Escolha uma palavra: ').strip().upper()
+        confirma.play()
+
+    # Modo 1 jogador: palavra escolhida aleatoriamente, o jogador escolhe a dificuldade.
+    else:
+        while True:
+            d = ['F', 'M', 'D', 'C']
+            dif = input('Dificuldade[F: Fácil][M: Médio][D: Difícil][C: Por Categoria]: ').strip().upper()
+            if dif in d:
+                dif = dif[0]
+                confirma.play()
+                break
+            print('Tente novamente.')
+            tentenovamente.play()
+
+        if dif == 'F':
+            plvr = choice(facil)
+        elif dif == 'M':
+            plvr = choice(medio)
+        elif dif == 'D':
+            plvr = choice(dificil)
+        else:
+            while True:
+                cat = ['1', '2', '3', '4', '5']
+                categoria = input('Escolha uma categoria: [1]Atores-Atrizes'' / '
+                                  '[2]Filmes / '
+                                  '[3]Animais / '
+                                  '[4]Países / '
+                                  '[5]Músicos-Bandas: ').strip()
+                if categoria in cat:
+                    categoria = categoria[0]
+                    confirma.play()
+                    break
+                print('Tente novamente.')
+                tentenovamente.play()
+
+            plvr = carregar_listas_categoria(categoria)
+
+    return plvr
+
+
+def coletar_palpite():
+    while True:
+        plpt = input('Letra: ').strip().upper()
+
+        if not plpt.isalpha() and plpt not in ['-', ';']:
+            print('Você não digitou uma letra. Tente novamente.')
+            tentenovamente.play()
+        return plpt[0]
+
+
+def exibir_forca(t):
     if t == 6:
         print("  _____")
         print(" |     |")
@@ -79,82 +167,77 @@ def tent(t):
         print("_|_")
 
 
-def logo():
+def exibir_logo():
     print(
-        "########################          --##########--        ####################--                @@################        --##########--")
+        "########################          --##########--        ####################--                "
+        "@@################        --##########--")
     sleep(0.81)
     print(
-        "########################      ######################    ##########################        ######################    ######################")
+        "########################      ######################    ##########################        "
+        "######################    ######################")
     sleep(0.81)
     print(
-        "########################    ########################@@  ##########################      ########################  ##########################")
+        "########################    ########################@@  ##########################      "
+        "########################  ##########################")
     sleep(0.81)
     print(
-        "########################    ##########################  ############################    ########################  ##########################")
+        "########################    ##########################  ############################    "
+        "########################  ##########################")
     sleep(0.81)
     print(
-        "########################  ############################  ############################  @@########################  ##########################")
+        "########################  ############################  ############################  "
+        "@@########################  ##########################")
     sleep(0.81)
     print(
-        "                                              ##########                    ##########  ##########++                                ##########++")
+        "##########                    ##########  ##########++                                ##########++")
     sleep(0.81)
     print(
-        "########################  ##########--      ##########  ############################  ##########                  ##########################++")
+        "########################  ##########--      ##########  ############################  ##########             "
+        "     ##########################++")
     sleep(0.81)
     print(
-        "########################  ##########--      ##########  ############################  ##########                  ##########################++")
+        "########################  ##########--      ##########  ############################  ##########             "
+        "     ##########################++")
     sleep(0.81)
     print(
-        "########################  ##########--      ##########  ##########################MM  ##########                  ##########################++")
+        "########################  ##########--      ##########  ##########################MM  ##########             "
+        "     ##########################++")
     sleep(0.81)
     print(
-        "########################  ##########--      ##########  ##########################    ##########                  ##########################++")
+        "########################  ##########--      ##########  ##########################    ##########             "
+        "     ##########################++")
     sleep(0.81)
     print(
-        "########################  ##########--      ##########  ######################        ##########                  ##########################++")
+        "########################  ##########--      ##########  ######################        ##########             "
+        "     ##########################++")
     sleep(0.81)
     print(
-        "##########                ##########--      ##########  ######################        ##########                  ##########      ##########++")
+        "##########                ##########--      ##########  ######################        ##########             "
+        "     ##########      ##########++")
     sleep(0.81)
     print(
-        "##########                ##########++      ##########  ##########  ############      ##########--                ##########      ##########++       Por LvEggers")
+        "##########                ##########++      ##########  ##########  ############      ##########--           "
+        "     ##########      ##########++       Por LvEggers")
     sleep(0.81)
     print(
-        "##########                MM##########################  ##########  @@##########      ##########################  ##########      ##########++")
+        "##########                MM##########################  ##########  @@##########      "
+        "##########################  ##########      ##########++")
     sleep(0.81)
     print(
-        "##########                  ##########################  ##########    ############      ########################  ##########      ##########++")
+        "##########                  ##########################  ##########    ############      "
+        "########################  ##########      ##########++")
     sleep(0.81)
     print(
-        "##########                  ##########################  ##########      ##########mm    ########################  ##########      ##########++MM######################")
+        "##########                  ##########################  ##########      ##########mm    "
+        "########################  ##########      ##########++MM######################")
     sleep(0.81)
     print(
-        "##########                    ######################    ##########      ############      ######################  ##########      ##########++MM######################")
+        "##########                    ######################    ##########      ############      "
+        "######################  ##########      ##########++MM######################")
     sleep(0.81)
     print(
-        "##########                        ############++        ##########        ##########MM        ##################  ##########      ##########++MM######################")
-
-
-def carregar_palavras_categoria(categ):
-    if categ == '1':
-        lista_cat = "listas/atores_atrizes.txt"
-    elif categ == '2':
-        lista_cat = "listas/filmes.txt"
-    elif categ == '3':
-        lista_cat = "listas/animais.txt"
-    elif categ == '4':
-        lista_cat = "listas/paises.txt"
-    else:
-        lista_cat = "listas/artistas_bandas.txt"
-
-    with open(lista_cat, 'r', encoding='utf-8') as palavras_cat:
-        lista_escolha = []
-        for linha in palavras_cat:
-            linha = linha.upper().strip()
-            lista_escolha.append(linha)
-        plvr = choice(lista_escolha)
-        palavras_cat.close()
-    return plvr
+        "##########                        ############++        ##########        ##########MM        "
+        "##################  ##########      ##########++MM######################")
 
 
 def verde(msg):
@@ -180,15 +263,15 @@ intro = pygame.mixer.Sound('Sons/intro.mp3')
 encerrar = pygame.mixer.Sound('Sons/encerrar.mp3')
 
 # intro.play()
-# logo()
+# exibir_logo()
 
 # Carrega as palavras e cria as listas das dificuldades.
-facil, medio, dificil = carregar_palavras()
+facil, medio, dificil = carregar_listas()
 
 sleep(2)
 pygame.mixer.music.play(-1)
 
-# Lista para converter caractere para caractere acentuado automaticamente.
+# Dicionário para converter caractere para caractere acentuado automaticamente.
 acento = {'A': ['Á', 'À', 'Ã', 'Â', 'Ä'],
           'E': ['É', 'Ê', 'È'],
           'I': ['Í'],
@@ -196,9 +279,9 @@ acento = {'A': ['Á', 'À', 'Ã', 'Â', 'Ä'],
           'U': ['Ú'],
           'C': ['Ç']}
 
-# Cria uma lista de emojis para erros e acertos.
-emo_acerto = ['😍', '😁', '🤗', '👏', '🙌', '🤞', '☺️']
-emo_erro = ['😬', '😱', '😰', '😓', '😭', '😨', '😖']
+# Dicionário de emojis para erros e acertos.
+emojis = {'acerto': ['😍', '😁', '🤗', '👏', '🙌', '🤞', '☺️'],
+          'erro': ['😬', '😱', '😰', '😓', '😭', '😨', '😖']}
 
 # guarda o número de vitórias consecutivas (modo 1 jogador)
 cont_vit = 0
@@ -207,89 +290,36 @@ with open('melhor.txt', 'r') as arquivo_melhor:
 
 # Início do programa.
 while True:
-    # Escolhe o número de jogadores.
-    while True:
-        md = ['1', '2']
-        modo = input('Escolha o número de jogadores [1/2]: ').strip()
-        if modo in md:
-            modo = modo[0]
-            confirma.play()
-            break
-        print('Tente novamente.')
-        tentenovamente.play()
-
-    # Modo 2 jogadores: um dos jogadores escolhe a palavra.
-    if modo == '2':
-        palavra = input('Escolha uma palavra: ').strip().upper()
-        confirma.play()
-
-    # Modo 1 jogador: palavra escolhida aleatoriamente, o jogador escolhe a dificuldade.
-    else:
-        while True:
-            d = ['F', 'M', 'D', 'C']
-            dif = input('Dificuldade[F: Fácil][M: Médio][D: Difícil][C: Por Categoria]: ').strip().upper()
-            if dif in d:
-                dif = dif[0]
-                confirma.play()
-                break
-            print('Tente novamente.')
-            tentenovamente.play()
-
-        if dif == 'F':
-            palavra = choice(facil)
-        elif dif == 'M':
-            palavra = choice(medio)
-        elif dif == 'D':
-            palavra = choice(dificil)
-        else:
-            while True:
-                cat = ['1', '2', '3', '4', '5']
-                categoria = input('Escolha uma categoria: [1]Atores-Atrizes'' / '
-                                  '[2]Filmes / '
-                                  '[3]Animais / '
-                                  '[4]Países / '
-                                  '[5]Músicos-Bandas: ').strip()
-                if categoria in cat:
-                    categoria = categoria[0]
-                    confirma.play()
-                    break
-                print('Tente novamente.')
-                tentenovamente.play()
-
-            palavra = carregar_palavras_categoria(categoria)
-
-    # Cria uma sequência de underlines, um para cada letra da palavra escolhida.
-    # Cria as listas tem/não tem, estabelece o número de tentativas.
+    # variáveis mutáveis:
     tentativas = 7
-    resp = ['_' for letra in palavra]
-    # Espaços não viram underline:
-    for c in range(len(palavra)):
-        if palavra[c] == ' ':
-            resp[c] = ' '
+    dar_dica = True
     lista_naotem = []
     lista_tem = []
 
-    # Começa o jogo / coleta e trata o palpite.
-    cont_dica = 0
+    # Escolhe o número de jogadores.
+    modo = num_jogadores()
+
+    # Carrega a palavra.
+    palavra = carregar_palavra()
+
+    # Cria underlines para cada letra da palavra selecionada, ignorando os espaços.
+    resp = [' ' if letra == ' ' else '_' for letra in palavra]
+
+    # Começa o jogo.
     while True:
         print(' '.join(resp))
-        palpite = input('Letra: ').strip().upper()
 
-        if not palpite.isalpha() and palpite not in ['-', ';']:
-            print('Você não digitou uma letra. Tente novamente.')
-            tentenovamente.play()
-            continue
-        else:
-            palpite = palpite[0]
+        # Coleta e trata o palpite.
+        palpite = coletar_palpite()
 
-        # implementa substituição automática (dica)
+        # Resolve o pedido por dica (substituição automática).
         if palpite == ';':
-            if cont_dica == 0:
+            if dar_dica:
                 lista_dica = [c for c in range(len(resp)) if resp[c] == '_']
                 subst = choice(lista_dica)
                 resp[subst] = palavra[subst]
                 segredo.play()
-                cont_dica += 1
+                dar_dica = False
                 continue
             else:
                 print('(Já te dei uma dica...)')
@@ -315,7 +345,7 @@ while True:
         if palpite in palavra or passe:  # O 'passe' serve para aceitar o caractere acentuado como se fosse sem acento.
             lista_tem.append(palpite)
             correto.play()
-            print(verde('Acertou!'), f'{choice(emo_acerto)}')
+            print(verde('Acertou!'), f'{choice(emojis["acerto"])}')
             for indice, letra in enumerate(palavra):
                 if letra == palpite:
                     resp[indice] = palpite
@@ -339,9 +369,9 @@ while True:
             erro.play()
             print(f'{lista_naotem}')
             tentativas -= 1
-            print(vermelho('Errou!'), f'{choice(emo_erro)}'
+            print(vermelho('Errou!'), f'{choice(emojis["erro"])}'
                                       f'\n Tentativas restantes: {tentativas}')
-            tent(tentativas)  # Mostra a forca
+            exibir_forca(tentativas)  # Mostra a forca
             if tentativas == 0:  # Condição de derrota.
                 print(vermelho('Você perdeu...'), f'☠️💔😢🥀💥 A palavra era {palavra}.')
                 pygame.mixer.music.stop()
